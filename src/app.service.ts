6bca9c5e9198b93383a38e32da5bfb0facba966e
Desenvolvimento <repository.deenvolvimento@gmail.com>
2021-12-14 17:44:06 -0300

    ARSENICUM-20211214173846

--- src/app.service.ts
+++ src/app.service.ts
@@ -4,7 +4,18 @@ import { Injectable } from '@nestjs/common';
 export class AppService {
 
   getHello(): string {
-    return 'ARSENICUM - Sistema de Gestao Financeira';
+    return `ARSENICUM-${this.retornarDataFormatoAAAAMMDDHHMMSS()}`;
+  }
+
+  private retornarDataFormatoAAAAMMDDHHMMSS(): string {
+    const data: Date = new Date();
+    const ano: string = data.getFullYear().toString().padStart(2,"0");
+    const mes: string = (data.getMonth()+1).toString().padStart(2,"0");
+    const dia: string = (data.getDate()).toString().padStart(2,"0");
+    const hora: string = data.getHours().toString();
+    const minuto: string = data.getMinutes().toString();
+    const segundo: string = data.getSeconds().toString();
+    return ano+mes+dia+hora+minuto+segundo;
   }
 
 }
