42a19cd59d271081d0eda6ecd04804e7ba295cd1
Sistema Financeiro <sistemafinanceiro@outlook.com.br>
2021-12-14 23:19:48 -0300

    ARSENICUM-20211214174653

--- arsenicumapi.d
+++ arsenicumapi.d
@@ -1,4 +1,4 @@
--- Projeto
+# Projeto
 
     npm install global @nestjs/cli
     nest new arsenicumapi
@@ -11,10 +11,21 @@
 
     nest generate class module/tipo-pessoa-module --no-spec
 
--- Dependências
+# Dependências
 
     npm install class-validator class-transformer
     npm install @nestjs/mapped-types
     npm install typeorm @nestjs/typeorm
     npm install pg
     npm install --save @nestjs/swagger swagger-ui-express
+
+    -- Email
+    npm i --save @anchan828/nest-sendgrid
+
+    -- Usar arquivo ENV
+    npm i --save @nestjs/config
+
+# Links
+
+    -- Envio de E-mail
+    https://sendgrid.com
--- src/app.module.ts
+++ src/app.module.ts
@@ -6,9 +6,10 @@ import { DatabaseConnection } from "./connection/database.connection";
 import { PessoaModule } from "./module/pessoa.module";
 import { PerfilModule } from "./module/perfil.module";
 import { UsuarioModule } from "./module/usuario.module";
+import { ConfigModule } from "@nestjs/config";
 
 @Module({
-  imports: [DatabaseConnection, TipoPessoaModule, PessoaModule, PerfilModule, UsuarioModule],
+  imports: [DatabaseConnection, ConfigModule.forRoot(), TipoPessoaModule, PessoaModule, PerfilModule, UsuarioModule],
   controllers: [AppController],
   providers: [AppService],
 })
