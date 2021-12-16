c2e21c9f44d737b5bb07ed02cad6afb4888bbe35
Sistema Financeiro <sistemafinanceiro@outlook.com.br>
2021-12-16 18:31:41 -0300

    ARSENICUM-20211214174653

--- src/connection/database-mysql.connection.ts
+++ src/connection/database-mysql.connection.ts
@@ -1,14 +1,17 @@
 import { Module } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
+import { ConfigModule } from "@nestjs/config";
 
 @Module({
-  imports: [TypeOrmModule.forRoot({
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRoot({
     type: "mysql",
-    host: "localhost",
-    port: 3306,
-    username: "root",
-    password: "arsenicum",
-    database: "db_arsenicum",
+    host: process.env.MYSQL_HOST,
+    port: parseInt(process.env.MYSQL_PORT),
+    username: process.env.MYSQL_USERNAME,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE,
     autoLoadEntities: true,
     synchronize: true
   })]
